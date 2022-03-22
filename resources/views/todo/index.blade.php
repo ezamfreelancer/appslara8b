2bd45bf7b330fe160937b27627a13a25e4a4961a
Shezam <ezam_freelancer@yahoo.com>
2022-03-22 10:00:15 +0800

    commit

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<br>
+  <div class="container">
+      <div class="row">
+          <div class="col">
+              <h1>Menu Buffet Ramadhan</h1>
+              <div class="card">
+                  <div class="card-body">
+                      <table class="table table-striped">
+                          <thead>
+                              <tr>
+                                  <th>#</th>
+                                  <th>Menu</th>
+                                  <th>Status</th>
+                                  <th>Action</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Siakap Tiga Rasa</td>
+                                <td>Not Done</td>
+                                <td><button class="btn btn-primary">Done</button></td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Kailan Ikan Masin</td>
+                                <td>Not Done</td>
+                                <td><button class="btn btn-primary">Done</button></td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Tomyam Campur</td>
+                                <td>Not Done</td>
+                                <td><button class="btn btn-primary">Done</button></td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Udang Celup Tepung</td>
+                                <td>Not Done</td>
+                                <td><button class="btn btn-primary">Done</button></td>
+                            </tr>
+                          </tbody>
+                      </table>
+
+                  </div>
+              </div>
+
+              <div class="card mt-5">
+              <div class="row text-white">
+                  <div class="col bg-primary">Menu Set A</div>
+                  <div class="col bg-success">Menu Set B</div>
+                  <div class="col bg-warning">Menu Set C</div>
+                  <div class="col bg-danger">Menu Set D</div>
+              </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+
+
+</body>
+</html>
